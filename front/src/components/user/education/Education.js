fbc97c2675575a1172c246e085c2360bb41a5047
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-04-28 00:11:31 +0900

    Fix : Education 정보 조작기능 오류수정
    - DB의 학위 목록을 가져와 나열할 시
    - 기존 코드에서 index의 충돌이 생기는 오류수정

--- front/src/components/user/education/Education.js
+++ front/src/components/user/education/Education.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Card, Row } from 'react-bootstrap';
 import EducationDetail from './EducationDetail';
 
-function EducationCreate() {
+function EducationCreate({ portfolioOwnerId }) {
     return (
         <Card style={{ width: '100%' }}>
             <Card.Title className='ms-3 mt-3'>학력</Card.Title>
             <Card.Body>
                 <Row>
                     <div>
-                        <EducationDetail />
+                        <EducationDetail portfolioOwnerId={portfolioOwnerId} />
                     </div>
                 </Row>
             </Card.Body>
--- front/src/components/user/education/EducationDetail.js
+++ front/src/components/user/education/EducationDetail.js
@@ -1,72 +1,156 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
-function EducationDetail() {
-    const [inputItems, setInputItems] = useState([]);
-    const [inputAddId, setInputAddId] = useState(0);
-    const [originItems, setOriginItems] = useState([]);
+import * as Api from '../../../api';
+
+function EducationDetail({ portfolioOwnerId }) {
+    const [dbItem, setDbItem] = useState([]);
+    const [toggle, setToggle] = useState(false); // 추가 버튼 클릭 유무
+    const [save, setSave] = useState(false); // 확인 버튼 클릭 유무
+    const [edit, setEdit] = useState(''); // 수정 버튼 클릭 유무
+
+    const [eduSchool, setEduSchool] = useState(''); // 학교이름
+    const [eduMajor, setEduMajor] = useState(''); // 전공
+    const [eduStart, setEduStart] = useState(''); // 입학일자
+    const [eduEnd, setEduEnd] = useState(''); // 졸업일자
+    const [eduDegree, setEduDegree] = useState(''); // 학위
+
+    const onChangeSchool = (e) => {
+        setEduSchool(e.target.value);
+    };
+
+    const onChangeMajor = (e) => {
+        setEduMajor(e.target.value);
+    };
+    const onChangeStart = (e) => {
+        setEduStart(e.target.value);
+    };
+
+    const onChangeEnd = (e) => {
+        setEduEnd(e.target.value);
+    };
+    const onChangeDegree = (e) => {
+        setEduDegree(e.target.value);
+    };
 
     const AddInput = () => {
-        const input = {
-            id: inputAddId,
-            eduSchool: '',
-            eduMajor: '',
-            eduStart: '',
-            eduEnd: '',
-            eduDegree: '',
-            isSubmit: false,
-            isEdit: false,
-        };
+        setToggle(true);
 
-        setInputItems((prevInputItems) => [...prevInputItems, input]);
-        setInputAddId((prevInputAddId) => prevInputAddId + 1);
+        setEduSchool('');
+        setEduMajor('');
+        setEduStart('');
+        setEduEnd('');
+        setEduDegree('');
     };
 
-    const handleSubmit = (index) => {
-        const newItem = { ...inputItems[index], isSubmit: true, isEdit: false };
-        const newItems = [...inputItems];
-        newItems.splice(index, 1, newItem);
-        setInputItems(newItems);
+    const certfetch = async (ownerId) => {
+        const { userId } = ownerId;
+        // 유저 id를 가지고 "/users/유저id" 엔드포인트로 요청해 사용자 정보를 불러옴.
+        const res = await Api.get('users', userId);
+        // 사용자 정보는 response의 data임.
+        const ownerData = res.data.certs;
+        // portfolioOwner을 해당 사용자 정보로 세팅함.
+        setDbItem(ownerData);
+        console.log(ownerData);
     };
 
-    const handleEdit = (index) => {
-        const newItem = { ...inputItems[index], isEdit: true };
-        const newItems = [...inputItems];
-        newItems.splice(index, 1, newItem);
-        setInputItems(newItems);
-        setOriginItems(inputItems);
-    };
+    const userId = portfolioOwnerId;
 
-    const handleDelete = (id, index) => {
-        if (!inputItems[index].isEdit) {
-            const newItems = inputItems.filter((item) => item.id !== id);
-            setInputItems(newItems);
+    useEffect(() => {
+        certfetch({ userId });
+    }, [userId, save]);
+
+    const handleSubmit = async (id) => {
+        const item = dbItem.filter((item) => item.id === id)[0];
+        if (item.isSave === false || !item) {
+            try {
+                // "cert/user_id" 엔드포인트로 post요청함.
+                await Api.post(`cert/${portfolioOwnerId}`, {
+                    eduSchool,
+                    eduMajor,
+                    eduStart,
+                    eduEnd,
+                    eduDegree,
+                });
+
+                setSave(true);
+                setToggle(false);
+
+                setEduSchool('');
+                setEduMajor('');
+                setEduStart('');
+                setEduEnd('');
+                setEduDegree('');
+            } catch (err) {
+                console.log('학위 추가에 실패하였습니다.', err);
+            }
         } else {
-            setInputItems(originItems);
+            try {
+                // "cert/user_id/cert_id" 엔드포인트로 put요청함.
+                await Api.put(`cert/${portfolioOwnerId}/${item.id}`, {
+                    eduSchool,
+                    eduMajor,
+                    eduStart,
+                    eduEnd,
+                    eduDegree,
+                });
+            } catch (err) {
+                console.log('학위 수정에 실패하였습니다.', err);
+            }
+        }
+    };
+
+    const handleEdit = (id) => {
+        const item = dbItem.filter((item) => item.id === id)[0];
+        item.isEdit = true;
+        setEduSchool(item.eduSchool);
+        setEduMajor(item.eduMajor);
+        setEduStart(item.eduStart);
+        setEduEnd(item.eduEnd);
+        setEduDegree(item.eduDegree);
+        setEdit(item.id);
+    };
+
+    const handleCancel = () => {
+        certfetch({ userId });
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            // "cert/user_id/cert_id" 엔드포인트로 delete 요청함.
+            await Api.delete(`cert/${portfolioOwnerId}/${id}`);
+
+            certfetch({ userId });
+        } catch (err) {
+            console.log('학위 삭제에 실패하였습니다.', err);
         }
     };
 
     return (
         <div>
-            {inputItems.map((item, index) => (
+            {dbItem.map((item) => (
                 <div key={item.id}>
-                    {item.isSubmit && item.isEdit === false ? (
+                    {item.isSave === true && item.isEdit === false ? (
                         <div>
                             <p>
-                                {item.eduSchool ? item.eduSchool : '미입력'}
+                                {item.eduSchool}
                                 <br />
-                                {item.eduMajor ? item.eduMajor : '미입력'}
+                                {item.eduMajor}
                                 <br />
-                                {item.eduStart ? item.eduStart : '미입력'}
+                                {item.eduStart}
                                 <br />
-                                {item.eduEnd ? item.eduEnd : '미입력'}
+                                {item.eduEnd}
                                 <br />
-                                {item.eduDegree ? item.eduDegree : '미입력'}
+                                {item.eduDegree}
                                 <br />
-                                <Button className='position-absolute end-0 translate-middle' variant='outline-primary' onClick={() => handleEdit(index)}>
-                                    Edit
-                                </Button>{' '}
                             </p>
+                            <br />
+                            <Button
+                                className='position-absolute end-0 translate-middle'
+                                variant='outline-primary'
+                                onClick={() => handleEdit(item.id)}>
+                                Edit
+                            </Button>{' '}
                             <br />
                             <hr className='one' />
                         </div>
@@ -77,73 +161,90 @@ function EducationDetail() {
                                     style={{ width: '100%' }}
                                     type='text'
                                     placeholder='학교이름'
-                                    value={item.eduSchool}
-                                    onChange={(e) =>
-                                        setInputItems((prevInputItems) => prevInputItems.map((prevItem) => (prevItem.id === item.id ? { ...prevItem, eduSchool: e.target.value } : prevItem)))
-                                    }
+                                    value={eduSchool}
+                                    onChange={onChangeSchool}
                                 />
                             </div>
                             <div className='mb-2'>
-                                <Form.Control
-                                    style={{ width: '100%' }}
-                                    type='text'
-                                    placeholder='전공'
-                                    value={item.eduMajor}
-                                    onChange={(e) =>
-                                        setInputItems((prevInputItems) => prevInputItems.map((prevItem) => (prevItem.id === item.id ? { ...prevItem, eduMajor: e.target.value } : prevItem)))
-                                    }
-                                />
+                                <Form.Control style={{ width: '100%' }} type='date' placeholder='전공' value={eduMajor} onChange={onChangeMajor} />
                             </div>
                             <div className='mb-2'>
                                 <Form.Control
                                     style={{ width: '100%' }}
                                     type='date'
                                     placeholder='입학일자'
-                                    value={item.eduStart}
-                                    onChange={(e) =>
-                                        setInputItems((prevInputItems) => prevInputItems.map((prevItem) => (prevItem.id === item.id ? { ...prevItem, eduStart: e.target.value } : prevItem)))
-                                    }
+                                    value={eduStart}
+                                    onChange={onChangeStart}
                                 />
                             </div>
                             <div className='mb-2'>
-                                <Form.Control
-                                    style={{ width: '100%' }}
-                                    type='date'
-                                    placeholder='졸업일자'
-                                    value={item.eduEnd}
-                                    onChange={(e) =>
-                                        setInputItems((prevInputItems) => prevInputItems.map((prevItem) => (prevItem.id === item.id ? { ...prevItem, eduEnd: e.target.value } : prevItem)))
-                                    }
-                                />
+                                <Form.Control style={{ width: '100%' }} type='date' placeholder='졸업일자' value={eduEnd} onChange={onChangeEnd} />
                             </div>
                             <div className='mb-2'>
-                                <Form.Control
-                                    style={{ width: '100%' }}
-                                    type='text'
-                                    placeholder='학위'
-                                    value={item.eduDegree}
-                                    onChange={(e) =>
-                                        setInputItems((prevInputItems) => prevInputItems.map((prevItem) => (prevItem.id === item.id ? { ...prevItem, eduDegree: e.target.value } : prevItem)))
-                                    }
-                                />
+                                <Form.Control style={{ width: '100%' }} type='date' placeholder='학위' value={eduDegree} onChange={onChangeDegree} />
                             </div>
                             <div className='mb-3 text-center'>
-                                <React.Fragment>
-                                    <Button key={item.id} variant='primary' onClick={() => handleSubmit(index)}>
-                                        확인
-                                    </Button>{' '}
-                                    <Button variant='secondary' onClick={() => handleDelete(item.id, index)}>
-                                        취소
-                                    </Button>{' '}
-                                </React.Fragment>
+                                {edit !== item.id ? (
+                                    <React.Fragment>
+                                        <Button variant='primary' onClick={() => handleSubmit(item.id)}>
+                                            확인
+                                        </Button>{' '}
+                                        <Button variant='secondary' onClick={() => handleCancel()}>
+                                            취소
+                                        </Button>{' '}
+                                    </React.Fragment>
+                                ) : (
+                                    <React.Fragment>
+                                        <Button variant='primary' onClick={() => handleSubmit(item.id)}>
+                                            확인
+                                        </Button>{' '}
+                                        <Button variant='danger' onClick={() => handleDelete(item.id)}>
+                                            삭제
+                                        </Button>{' '}
+                                        <Button variant='secondary' onClick={() => handleCancel()}>
+                                            취소
+                                        </Button>{' '}
+                                    </React.Fragment>
+                                )}
                             </div>
                         </div>
                     )}
                 </div>
             ))}
+            {toggle === true ? (
+                <div>
+                    <div className='mb-2'>
+                        <Form.Control style={{ width: '100%' }} type='text' placeholder='학교이름' value={eduSchool} onChange={onChangeSchool} />
+                    </div>
+                    <div className='mb-2'>
+                        <Form.Control style={{ width: '100%' }} type='date' placeholder='전공' value={eduMajor} onChange={onChangeMajor} />
+                    </div>
+                    <div className='mb-2'>
+                        <Form.Control style={{ width: '100%' }} type='date' placeholder='입학일자' value={eduStart} onChange={onChangeStart} />
+                    </div>
+                    <div className='mb-2'>
+                        <Form.Control style={{ width: '100%' }} type='date' placeholder='졸업일자' value={eduEnd} onChange={onChangeEnd} />
+                    </div>
+                    <div className='mb-2'>
+                        <Form.Control style={{ width: '100%' }} type='date' placeholder='학위' value={eduDegree} onChange={onChangeDegree} />
+                    </div>
+                    <div className='mb-3 text-center'>
+                        <React.Fragment>
+                            <Button variant='primary' onClick={() => handleSubmit()}>
+                                확인
+                            </Button>{' '}
+                            <Button variant='secondary' onClick={() => handleCancel()}>
+                                취소
+                            </Button>{' '}
+                        </React.Fragment>
+                    </div>
+                </div>
+            ) : (
+                ''
+            )}
 
             <div className='mb-3 text-center'>
-                {inputItems.length < 10 && (
+                {dbItem.length < 10 && (
                     <Button variant='primary' onClick={AddInput}>
                         +
                     </Button>
