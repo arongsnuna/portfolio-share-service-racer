1a0e329228af656a0ee0908687af56f8bd9200ab
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-04-30 02:02:34 +0900

    Refactor : EducationDetail.js 리팩토링

---
+++ front/src/components/user/education/EducationForm.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+function EducationForm({ formSendFunction, currentData, isFlag, item }) {
+    const { handleSubmit, handleCancel, handleDelete, onChangeSchool, onChangeMajor, onChangeEnter, onChangeGraduate, onChangeDegree } =
+        formSendFunction;
+    const { eduSchool, eduMajor, eduEnter, eduGraduate, eduDegree, currentEditId } = currentData;
+    const { isDateValid } = isFlag;
+
+    return (
+        <div>
+            <div className='mb-2'>
+                <Form.Control style={{ width: '100%' }} type='text' placeholder='학교이름' value={eduSchool} onChange={onChangeSchool} />
+            </div>
+            <div className='mb-2'>
+                <Form.Control style={{ width: '100%' }} type='text' placeholder='전공' value={eduMajor} onChange={onChangeMajor} />
+            </div>
+            <div className='mb-2'>
+                <Form.Control style={{ width: '100%' }} type='date' placeholder='입학일자' value={eduEnter} onChange={onChangeEnter} />
+            </div>
+            <div className='mb-2'>
+                <Form.Control style={{ width: '100%' }} type='date' placeholder='졸업일자' value={eduGraduate} onChange={onChangeGraduate} />
+                {eduEnter && eduGraduate && !isDateValid && <Form.Text className='date-success'>입학날짜보다 졸업일자가 이전입니다.</Form.Text>}
+            </div>
+            <div className='mb-2'>
+                <Form.Control style={{ width: '100%' }} type='text' placeholder='학위' value={eduDegree} onChange={onChangeDegree} />
+            </div>
+            <div className='mb-3 text-center'>
+                {currentEditId !== item._id ? (
+                    <React.Fragment>
+                        <Button variant='primary' onClick={() => handleSubmit(item._id)}>
+                            확인
+                        </Button>
+                        <Button variant='secondary' onClick={() => handleCancel()}>
+                            취소
+                        </Button>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <Button variant='primary' onClick={() => handleSubmit(item._id)}>
+                            확인
+                        </Button>
+                        <Button variant='danger' onClick={() => handleDelete(item._id)}>
+                            삭제
+                        </Button>
+                        <Button variant='secondary' onClick={() => handleCancel()}>
+                            취소
+                        </Button>
+                    </React.Fragment>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default EducationForm;
