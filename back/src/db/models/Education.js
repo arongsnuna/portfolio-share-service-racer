964a14db0e7a2aaf7206baf0323cc7adb6285d4a
arongsnuna <unhi000@naver.com>
2023-04-30 21:54:41 +0900

    Fix: Education MVP 모델 사용하도록 수정

--- back/src/db/models/Education.js
+++ back/src/db/models/Education.js
@@ -1,68 +1,40 @@
 import { UserModel } from '../schemas/user';
 import { EducationModel } from '../schemas/education';
 // crud
-// eduSchool, eduMajor, eduStart, eduEnd, eduDegree
+// eduSchool, eduMajor, eduEnterDate, eduGraduateDate, eduDegree
 
 class Education {
     // 특정 유저의 전체 학력 조회
     static async findAll({ user_id }) {
-        const user = await UserModel.findById({ userId: user_id }); // id를 뭐라 고쳐야되지?
-        if (!user) {
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-        return user.educations;
+        const user = await UserModel.findOne({ id: user_id }); 
+        const educations = await EducationModel.find({userId: user._id});
+        return educations;
     }
     // 특정 유저의 특정 학력 조회
-    static async findById({ user_id }) {
-        const user = await UserModel.findOne({ id: user_id }); //모르게써
-        return user;
+    static async findById({ education_id }) {
+        const education = await EducationModel.findById({_id: education_id});
+        return education;
     }
 
     // 특정 유저의 학력 추가
-    static async create({ user_id, id, eduSchool, eduMajor, eduStart, eduEnd, eduDegree }) {
+    static async create({ user_id, eduSchool, eduMajor, eduEnterDate, eduGraduateDate, eduDegree }) {
         const user = await UserModel.findOne({ id: user_id });
-        const newEducation = { id, eduSchool, eduMajor, eduStart, eduEnd, eduDegree };
-
-        if (!user) {
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-        user.educations.push(newEducation);
-
-        await user.save();
-        return user;
+        const createdEducation = await EducationModel.create({eduSchool, eduMajor, eduEnterDate, eduGraduateDate, eduDegree, userId: user._id});
+        return createdEducation;
     }
 
     // 특정 유저의 학력 수정
-    static async update({ user_id, education_id, fieldToUpdate, newValue }) {
-        const user = await UserModel.findById({ user_id });
-        if (!user) {
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-        const education = user.educations.id(education_id);
-
-        if (!education) {
-            throw new Error(`${education_id} 학력은 존재하지 않습니다.`);
-        }
-
-        education[fieldToUpdate] = newValue;
-        await user.save();
-        return education;
+    static async update({ user_id, education_id,eduSchool, eduMajor, eduEnterDate, eduGraduateDate, eduDegree }) {
+        const user = await UserModel.findOne({ id: user_id });
+        const updatedEducation = await EducationModel.updateOne({userId: user._id, _id: education_id},{eduSchool, eduMajor, eduEnterDate, eduGraduateDate, eduDegree});
+        return updatedEducation;
     }
 
     // 특정 유저의 학력 삭제
     static async delete({ user_id, education_id }) {
-        const user = await UserModel.findById({ user_id });
-        if (!user) {
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-
-        const education = user.educations.id(education_id);
-        if (!education) {
-            throw new Error('이 학력 정보는 존재하지 않습니다.');
-        }
-        education.remove();
-        await user.save();
-        return education;
+        const user = await UserModel.findOne({ id: user_id });
+        const deletedEducation = await EducationModel.deleteOne({_id: education_id, userId: user._id});
+        return deletedEducation;
     }
 }
 
