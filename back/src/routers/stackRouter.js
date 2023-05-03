625eafc26109cf4e7e0d19857e760fc3bef9ab8a
arongsnuna <unhi000@naver.com>
2023-05-03 12:05:33 +0900

    Refactor: Stack MVP 검증 수행 단계 수정 및 프론트에서 alert 창 띄우기

--- back/src/routers/stackRouter.js
+++ back/src/routers/stackRouter.js
@@ -10,6 +10,10 @@ stackRouter.get('/', async(req,res,next)=>{
 
     try{
         const stacks = await stackService.findAll({ user_id });
+        if (!stacks){
+            res.status(400).send({error: '유저의 스택 정보가 존재하지 않습니다.'})
+            throw new Error(`${user_id} 유저의 스택 정보가 존재하지 않습니다.`);
+        }
         res.status(200).json(stacks)
     }catch(error){
         next(error);
@@ -20,15 +24,26 @@ stackRouter.get('/', async(req,res,next)=>{
 stackRouter.post('/', async(req, res, next) => {
     try{
         if(is.emptyObject(req.body)){
+            res.status(400).send(error);
             throw new Error('headers의 Content-Type을 application/json으로 설정해주세요');
         }
 
         const user_id = req.currentUserId;
-        const {stackName, stackDescription} = req.body;
 
+        const stacks = await stackService.findAll({ user_id });
+
+        const {stackName, stackDescription} = req.body;
         const newStack = {stackName, stackDescription};
+        
+        const stackExists = stacks.some((stack) => stack.stackName === newStack.stackName);
+        if (stackExists ){
+            res.status(400).send({error: `${newStack.stackName} 프로젝트는 이미 존재합니다.`})
+            throw new Error(`${newStack.stackName} 프로젝트는 이미 존재합니다.`);
+        }
+        
 
         if(!stackName || !stackDescription){
+            res.status(400).send({error: '모든 값을 입력했는지 확인해주세요.'})
             throw new Error('모든 값을 입력했는지 확인해주세요.');
         }
 
@@ -53,10 +68,23 @@ stackRouter.put('/:stack_id', async(req, res, next) => {
         const { stackName, stackDescription } = req.body;
 
         if(!stackName || !stackDescription){
+            res.status(400).send({error: '모든 값을 입력했는지 확인해주세요.'});
             throw new Error('모든 값을 입력했는지 확인해주세요.');
         }
 
         const newStack = {stackName, stackDescription};
+        const stack = await stackService.findOne({stack_id});
+        if (!stack) {
+            res.status(400).send({error: '이 스택 정보는 존재하지 않습니다.'});
+            throw new Error('이 스택 정보는 존재하지 않습니다.');
+        }
+        // 수정하려는 스택만 제외하고 다른 모든 스택을 가져올 수 있는 방법
+        const exceptStacks = await stackService.findExcept({ user_id, stack_id });
+        const stackExists = exceptStacks.some((stack) => stack.stackName === newStack.stackName);
+        if (stackExists ){
+            res.status(400).send({error: `${newStack.stackName} 프로젝트는 이미 존재합니다.`})
+            throw new Error(`${newStack.stackName} 프로젝트는 이미 존재합니다.`);
+        }
         const updatedStack = await stackService.updateStack({ user_id, stack_id, newStack });
         res.status(200).json(updatedStack);
     }catch(error){
@@ -70,6 +98,12 @@ stackRouter.delete('/:stack_id', async(req,res,next) => {
         const user_id = req.currentUserId;
         const { stack_id } = req.params;
 
+        const stack = await stackService.findOne({stack_id});
+        if(!stack){
+            res.status(400).send({error: '이 스택 정보는 존재하지 않습니다.'})
+            throw new Error('이 스택 정보는 존재하지 않습니다.');
+        }
+        
         const deletedStack = await stackService.deleteStack({ user_id, stack_id });
         res.status(200).json(deletedStack);
 
--- back/src/services/stackService.js
+++ back/src/services/stackService.js
@@ -3,33 +3,25 @@ import { User, Stack} from '../db';
 class stackService{
     // 전체 스택 정보 조회
     static async findAll({ user_id }){
-        const user = await User.findById({ user_id });
-
-        if (!user){
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
         const stacks = await Stack.findAll({ user_id });
-        if (!stacks){
-            throw new Error(`${user_id} 유저의 스택 정보가 존재하지 않습니다.`);
-        }
+        
         return stacks;
+    }
+    // 개별 스택 정보 조회
+    static async findOne({ stack_id}){
+        const stack = await Stack.findById({ stack_id });
+        return stack;
+    }
+    // 특정 스택 제외하고 모든 스택 조회
+    static async findExcept({ user_id, stack_id }){
+        const stacks = await Stack.findAll({ user_id });
+        const exceptStacks = stacks.filter((stack)=>stack.stack_id === stack_id);
+        return exceptStacks;
     }
     // 스택 정보 추가
     static async createStack({ user_id, newStack}){
-        const user = await User.findById({ user_id: user_id});
-        if(!user){
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-
-        if (user.id !== user_id) {
-            throw new Error(`스택 정보를 추가할 수 있는 권한이 없습니다.`);
-        }
-
-        const stacks = await Stack.findAll({ user_id });
-        const stackExists = stacks.some((stack) => stack.stackName === newStack.stackName);
-        if (stackExists ){
-            throw new Error(`${newStack.stackName} 프로젝트는 이미 존재합니다.`);
-        }
+        
+        // 스택 정보 추가 권한 제한은 프론트를 통해 막아둠
 
         const {stackName, stackDescription} = newStack;
         const createdStack = await Stack.create({ user_id, stackName, stackDescription });
@@ -39,24 +31,8 @@ class stackService{
 
     // 스택 정보 수정
     static async updateStack({ user_id, stack_id, newStack }){
-        const user = await User.findById({ user_id: user_id });
-        if(!user){
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-        if (user.id !== user_id){
-            throw new Error('스택 정보를 수정할 수 있는 권한이 없습니다.');
-        }
-
-        const stack = await Stack.findById({ stack_id, userId: user_id });
-        if (!stack) {
-            throw new Error('이 스택 정보는 존재하지 않습니다.');
-        }
-        const stacks = await Stack.findAll({ user_id });
-        const stackExists = stacks.some((stack) => stack.stackName === newStack.stackName);
-        if (stackExists ) {
-            throw new Error(`${newStack.stackName} 스택은 이미 존재합니다.`);
-        }
-
+        
+        // 스택 정보 수정 권한 제한은 프론트를 통해 막아둠
         const { stackName, stackDescription } = newStack;
         const updatedStack = await Stack.update({ user_id, stack_id, stackName, stackDescription });
         return updatedStack;
@@ -64,19 +40,7 @@ class stackService{
 
     // 스택 정보 삭제
     static async deleteStack({ user_id, stack_id }){
-        const user = await User.findById({ user_id: user_id});
-        if (!user) {
-            throw new Error(`${user_id} 유저는 존재하지 않습니다.`);
-        }
-
-        if (user.id !== user_id) {
-            throw new Error('스택 정보를 삭제할 수 있는 권한이 없습니다.');
-        }
-
-        const stack = await Stack.findById({ stack_id });
-        if(!stack){
-            throw new Error('이 스택 정보는 존재하지 않습니다.');
-        }
+        // 스택 정보 삭제 권한 제한은 프론트를 통해 막아둠
 
         const deletedStack = await Stack.delete({ user_id, stack_id });
 
