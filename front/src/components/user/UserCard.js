e7377631336020cbc55ae15d767ebde11487ee8c
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-05-06 00:45:01 +0900

    GitLink 이동 시 https 제거

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -9,30 +9,34 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork, userImageUrl }) {
     }
 
     return (
-        <Card className='mb-5 ms-5 mr-5' style={{ width: '18rem', borderRadius: '18px' }}>
+        <Card className="mb-5 ms-5 mr-5" style={{ width: '18rem', borderRadius: '18px' }}>
             <Card.Body>
-                <Row className='justify-content-md-center'>
+                <Row className="justify-content-md-center">
                     <Card.Img
                         style={{ width: '10rem', height: '8rem' }}
-                        className='mb-3 mt-1'
+                        className="mb-3 mt-1"
                         // src='http://placekitten.com/200/200'
                         src={userImageUrl}
-                        alt='랜덤 고양이 사진 (http://placekitten.com API 사용)'
+                        alt="랜덤 고양이 사진 (http://placekitten.com API 사용)"
                     />
                 </Row>
                 <Card.Title style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>{user?.name}</Card.Title>
-                <Card.Subtitle className='mb-1 text-muted' style={{ fontSize: '0.8rem' }}>{user?.email}</Card.Subtitle>
-                
+                <Card.Subtitle className="mb-1 text-muted" style={{ fontSize: '0.8rem' }}>
+                    {user?.email}
+                </Card.Subtitle>
+
                 {(!user?.gitLink || user?.gitLink !== 'undefined') && (
-                    <Card.Link href={`https://${user?.gitLink}`} className='mb-3 text-muted' style={{ fontSize: '0.8rem' }}>{user?.gitLink}</Card.Link>
+                    <Card.Link href={`${user?.gitLink}`} className="mb-3 text-muted" style={{ fontSize: '0.8rem' }}>
+                        {user?.gitLink}
+                    </Card.Link>
                 )}
-                <Card.Text className='mb-2 mt-2 text-muted'>{user?.description}</Card.Text>
+                <Card.Text className="mb-2 mt-2 text-muted">{user?.description}</Card.Text>
 
                 {isEditable && (
                     <Col>
-                        <Row className='mt-3 text-center text-info'>
+                        <Row className="mt-3 text-center text-info">
                             <Col sm={{ span: 20 }}>
-                                <Button variant='outline-info' size='sm' onClick={() => setIsEditing(true)}>
+                                <Button variant="outline-info" size="sm" onClick={() => setIsEditing(true)}>
                                     편집
                                 </Button>
                             </Col>
@@ -42,7 +46,7 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork, userImageUrl }) {
 
                 {isNetwork && (
                     <Row>
-                        <Card.Link className='mt-3' href='#' onClick={() => navigate(`/user/${user._id}`)}>
+                        <Card.Link className="mt-3" href="#" onClick={() => navigate(`/user/${user._id}`)}>
                             포트폴리오
                         </Card.Link>
                     </Row>
