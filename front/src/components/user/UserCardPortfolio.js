53181017587de5c0a6e07a71a09bf0a4bd1a9932
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-05-06 00:00:14 +0900

    GitLink에서 Link로 수정

--- front/src/components/user/UserCardPortfolio.js
+++ front/src/components/user/UserCardPortfolio.js
@@ -13,37 +13,37 @@ function UserCardPortfolio({ user, setIsEditing, isEditable, isNetwork, userImag
     };
 
     return (
-        <Row xs='auto'>
+        <Row xs="auto">
             <Col>
                 <Card.Img
                     style={{ width: '10rem', height: '10rem', borderRadius: '70%', overflow: 'hidden' }}
-                    className='mb-3 mt-3 ms-3'
+                    className="mb-3 mt-3 ms-3"
                     src={userImageUrl}
-                    alt='랜덤 고양이 사진 (http://placekitten.com API 사용)'
+                    alt="랜덤 고양이 사진 (http://placekitten.com API 사용)"
                 />
             </Col>
-            <Col className='d-flex flex-column justify-content-center'>
-                <Row xs='auto' className='justify-content-left'>
+            <Col className="d-flex flex-column justify-content-center">
+                <Row xs="auto" className="justify-content-left">
                     <Col>
                         <Card.Title>{user?.name}</Card.Title>
                     </Col>
                     <Col>
-                        <Card.Subtitle className='mt-1 text-muted'>{user?.email}</Card.Subtitle>
+                        <Card.Subtitle className="mt-1 text-muted">{user?.email}</Card.Subtitle>
                     </Col>
                 </Row>
                 <Row>
-                    <Card.Text className='mb-2 text-muted'>{user?.description}</Card.Text>
+                    <Card.Text className="mb-2 text-muted">{user?.description}</Card.Text>
                 </Row>
             </Col>
-            <Col sm className='mt-3 text-end text-info'>
+            <Col sm className="mt-3 text-end text-info">
                 {(!user?.gitLink || user?.gitLink !== 'undefined') && (
                     <Button style={{ backgroundColor: '#2a3741', border: 'none', width: '80px' }} onClick={handleClick}>
-                        GitLink
+                        Link
                     </Button>
                 )}
                 {isEditable && (
                     <Col>
-                        <Row className='mt-3 text-end text-info'>
+                        <Row className="mt-3 text-end text-info">
                             <Col>
                                 <Button
                                     style={{ backgroundColor: '#2a3741', border: 'none', width: '80px' }}
@@ -57,7 +57,7 @@ function UserCardPortfolio({ user, setIsEditing, isEditable, isNetwork, userImag
 
                 {isNetwork && (
                     <Row>
-                        <Card.Link className='mt-3' href='#' onClick={() => navigate(`/user/${user._id}`)}>
+                        <Card.Link className="mt-3" href="#" onClick={() => navigate(`/user/${user._id}`)}>
                             포트폴리오
                         </Card.Link>
                     </Row>
